b45597d4bff76fd5e591787c65848ac58afb7937
Sebastian Sardina <ssardina@gmail.com>
2018-10-12 11:52:02 +1100

    Clean up and extended standalone JUNIT test cases

--- src/java/org/jpl7/test/standalone/quoted_name.pl
+++ src/java/org/jpl7/test/standalone/quoted_name.pl
@@ -1,10 +1,4 @@
 :- module(jpl).
 
-
 quoted_name(Atom, Quoted) :-
-    write("Here is the ATOM in Prolog the quoted name: "), writeln(Atom),
-    format(atom(Quoted), '~q', [Atom]),
-    write("Here is in Prolog the QUOTED ATOM: "), writeln(Quoted).
-
-
-test :- writeln("THIS IS A TEST FROM PROLOG").
+    format(atom(Quoted), '~q', [Atom]).
