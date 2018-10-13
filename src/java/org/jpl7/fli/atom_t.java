535d9216598f60c55985e95abf44fa278d09352b
Sebastian Sardina <ssardina@gmail.com>
2018-10-13 18:18:14 +1100

    Changed @author in files to avoid warnings in Java 10 SE

--- src/java/org/jpl7/fli/atom_t.java
+++ src/java/org/jpl7/fli/atom_t.java
@@ -33,7 +33,7 @@ package org.jpl7.fli;
  * POSSIBILITY OF SUCH DAMAGE.
  * <hr>
  * 
- * @author Fred Dushin <fadushin@syr.edu>
+ * @author Fred Dushin fadushin@syr.edu
  * @version $Revision$
  */
 public class atom_t extends LongHolder {
